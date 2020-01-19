e3bf42b2b552e7eb95ed870a93e529387b7015f1
Luke.S.Murray <56583742+Lukusmanucus@users.noreply.github.com>
2020-01-19 22:47:43 +0100

    MA2
    I hope this is correct and worth of an A++

---
+++ script.js
@@ -0,0 +1,59 @@
+// question 1
+
+var petArray = [
+    "cat",
+    "cow",
+    "dog"
+];
+
+// question 2
+
+console.log(petArray[1]);
+
+// question 3
+
+petArray.push ("sheep");
+
+// question 4
+
+console.log(petArray.length);
+
+// question 5
+
+var catObject = {
+
+    name: "Fuzzball",
+    color: "Blue/Gold",
+    age: 5
+};
+
+// question 6
+
+console.log(catObject.age);
+
+// question 7
+
+var catArray = [
+    {
+        name: "Fuzzball",
+        color: "Blue/Gold",
+        age: 5
+    }
+];
+
+//question 8
+
+for(var i = 0; i < catArray.length; i++) {
+
+    console.log(catArray[i]);
+}
+
+// question 9
+
+function logToConsole(animalName) {
+    console.log(animalName);
+}
+
+// question 10
+
+logToConsole("donkey");
